f2bcbef83830fafc45f54d143f40fe79d50db2f8
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-09-27 15:16:10 -0300

    Update Teste_ConversaoDeUnidadesDeArea.java

--- Teste_ConversaoDeUnidadesDeArea.java
+++ Teste_ConversaoDeUnidadesDeArea.java
@@ -5,7 +5,21 @@ import static org.junit.jupiter.api.Test;
 @DisplayName("Classe para teste de conversão de unidades")
 public class Teste_ConversaoDeUnidadesDeArea {
     @Test
-    public void Conversao_Test(){
+    public void Conversao_Test_Pe(){
+        double x = ConversaoDeUnidadesDeArea.Metro_Para_Pe(8250);
+        double y = ConversaoDeUnidadesDeArea.Pe_Para_Acre(x);
+        double z = ConversaoDeUnidadesDeArea.Pe_Para_Cm(x);
+        Assertions.assertEquals(z, 88770.0);
+    }
+    @Test
+    public void Conversao_Test_Acre(){
+        double x = ConversaoDeUnidadesDeArea.Metro_Para_Pe(8250);
+        double y = ConversaoDeUnidadesDeArea.Pe_Para_Acre(x);
+        double z = ConversaoDeUnidadesDeArea.Pe_Para_Cm(x);
+        Assertions.assertEquals(y, 2037.8787878787878);
+    }
+    @Test
+    public void Conversao_Test_cm(){
         double x = ConversaoDeUnidadesDeArea.Metro_Para_Pe(8250);
         double y = ConversaoDeUnidadesDeArea.Pe_Para_Acre(x);
         double z = ConversaoDeUnidadesDeArea.Pe_Para_Cm(x);
