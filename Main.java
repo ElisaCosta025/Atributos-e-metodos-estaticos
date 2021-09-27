c79419e08e3a35d8e0ed3ac08ef44df9628dca3c
ElisaCosta025 <63672767+ElisaCosta025@users.noreply.github.com>
2021-09-27 15:12:52 -0300

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-class Main {
-  public static void main(String[] args) {
-    double x = ConversaoDeUnidadesDeArea.Metro_Para_Pe(8250);
-		double y = ConversaoDeUnidadesDeArea.Pe_Para_Acre(x);
-		double z = ConversaoDeUnidadesDeArea.Pe_Para_Cm(x);
-		System.out.println("Em pés: "+ x +" Em Acres: "+ y +" Em cm: "+ z);
-  }
-}
